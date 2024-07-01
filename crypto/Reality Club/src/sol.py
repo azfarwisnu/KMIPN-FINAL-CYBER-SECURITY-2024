5a875bf68c9a20809970dba7a3ce3c1365501664
j0ssie <jossie@tennet.id>
2024-07-02 00:27:54 +0700

    woi

--- crypto/Reality Club/src/sol.py
+++ crypto/Reality Club/src/sol.py
@@ -1,7 +1,8 @@
 from pwn import *
 from Crypto.Util.number import *
 
-f=process(['python3','soal.py'])
+# f=process(['python3','soal.py'])
+f=remote('182.3.51.136','4423')
 payload=b'a'*100
 f.sendlineafter(b'> ',b'1')
 f.sendlineafter(b'> ',payload)
